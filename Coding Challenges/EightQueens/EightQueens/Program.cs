9184177d2ccf890581d38eb528a90cb917329eaa
TechnicallyMay <masonwells01@gmail.com>
2019-03-08 16:42:02 -0700

    Added methods for converting inputs to workable types

--- Coding Challenges/EightQueens/EightQueens/Program.cs
+++ Coding Challenges/EightQueens/EightQueens/Program.cs
@@ -1,25 +1,32 @@
-﻿//Have the function EightQueens(strArr) read strArr which will be an array consisting of the locations of eight Queens on a standard 8x8 
-//chess board with no other pieces on the board.The structure of strArr will be the following: ["(x,y)", "(x,y)", ...] where (x, y) 
-//represents the position of the current queen on the chessboard (x and y will both range from 1 to 8 where 1,1 is the bottom-left of the
-//chessboard and 8,8 is the top-right). Your program should determine if all of the queens are placed in such a way where none of them
-//are attacking each other. If this is true for the given input, return the string true otherwise return the first queen in the list that 
-//is attacking another piece in the same format it was provided. 
-
-
-using System;
+﻿using System;
+using System.Drawing;
 
 class MainClass
 {
-    private static string EightQueens(string[] strArr)
-    {   
-        foreach (string coord in strArr)
-        {
-            int x = (int)Char.GetNumericValue(coord[1]);
-            int y = (int)Char.GetNumericValue(coord[3]);
-            Console.WriteLine("{0}, {1}", x, y);
-        }
+    //Converts char to int
+    private static int CharToInt(char character) => (int)Char.GetNumericValue(character);
 
-        return strArr[0];
+    //Converts the input format "(x,y)" to a Point object
+    private static Point StringToPoint(string coords)
+    {
+        Point pointCoords = new Point(CharToInt(coords[1]), CharToInt(coords[3]));
+        return pointCoords;
+    }   
+
+    private static Point[] StrArrToPointArr(string[] strArr)
+    {
+        Point[] pointArr = new Point[8];
+        for (int i = 0; i < 8; i++)
+        {
+            pointArr[i] = StringToPoint(strArr[i]);
+        }
+        return pointArr;
+    }
+
+    private static string EightQueens(string[] strArr)
+    {
+        Point[] pointArr = StrArrToPointArr(strArr); 
+        return "true";
 
     }
 
