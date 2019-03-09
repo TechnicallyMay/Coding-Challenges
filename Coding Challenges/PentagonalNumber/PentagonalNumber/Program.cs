9f742490b9a9a12479861c59fa2696ee72fba194
TechnicallyMay <masonwells01@gmail.com>
2019-03-08 17:32:30 -0700

    Initial Commit

---
+++ Coding Challenges/PentagonalNumber/PentagonalNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class MainClass
+{
+    public static int PentagonalNumber(int num)
+    {
+        int output = 1;
+        for (int i = 1; i <= num; i++)
+        {
+            output += 5 * (i-1);
+        }
+        return output;
+    }
+
+    static void Main()
+    {
+        for (int i = 1; i < 20; i++)
+        {
+            Console.WriteLine(PentagonalNumber(i));
+        }
+        Console.ReadLine();
+    }
+}
