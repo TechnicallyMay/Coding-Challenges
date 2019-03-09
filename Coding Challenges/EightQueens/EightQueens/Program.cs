d78c20dd18c0f5faf28e729312b9daaf5e4a2142
TechnicallyMay <masonwells01@gmail.com>
2019-03-08 17:09:18 -0700

    Finished

--- Coding Challenges/EightQueens/EightQueens/Program.cs
+++ Coding Challenges/EightQueens/EightQueens/Program.cs
@@ -13,6 +13,7 @@ class MainClass
         return pointCoords;
     }   
 
+    //Converts original input to array of points
     private static Point[] StrArrToPointArr(string[] strArr)
     {
         Point[] pointArr = new Point[8];
@@ -23,18 +24,43 @@ class MainClass
         return pointArr;
     }
 
+    //Checks if one queen can attack another (does not check diagonal movement
+    //because not possible to attack diagonally without also being able to attack
+    //laterally
+    private static bool CheckAttack(Point point, Point other)
+    {
+        if (point.X == other.X || point.Y == other.Y)
+        {
+            return true;
+        }
+        return false;
+    }
+
     private static string EightQueens(string[] strArr)
     {
-        Point[] pointArr = StrArrToPointArr(strArr); 
+        Point[] pointArr = StrArrToPointArr(strArr);
+        foreach (Point point in pointArr)
+        {
+            foreach (Point other in pointArr)
+            {
+                if (point != other)
+                {
+                    if (CheckAttack(point, other))
+                    {
+                        string coords = $"{point.X},{point.Y}";
+                        return coords;
+                    }
+                }
+            }
+        }
         return "true";
-
     }
 
     static void Main()
     {
-        string[] input = new string[] { "(2,1)", "(4,3)", "(6,3)", "(8,4)", "(3,4)", "(1,6)", "(7,7)", "(5,8)" };
-        Console.WriteLine(EightQueens(input));
+        string[] failing = new string[] {"(2,1)", "(4,3)", "(6,3)", "(8,4)", "(3,4)", "(1,6)", "(7,7)", "(5,8)"};
+        string[] passing = new string[] {"(2,1)", "(4,2)", "(6,3)", "(8,4)", "(3,5)", "(1,6)", "(7,7)", "(5,8)"};
+        Console.WriteLine(EightQueens(failing));
         Console.ReadLine();
     }
-
 }
